974e62b099bd1529ec5dc05570d53f99fc41e2fb
Gomozov <Valeriy.gomozov@gmail.com>
2012-07-22 20:12:29 +0400

    Story with State_machine

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -17,4 +17,13 @@ class StoriesController < ApplicationController
     end                                                  
   end                                                        
 
+def update                 
+  @story = Story.find(params[:id])
+  if @story.update_attributes(params[:story])
+    redirect_to @story.project
+  else                                 
+    render @story.project
+  end  
+end                        
+
 end
